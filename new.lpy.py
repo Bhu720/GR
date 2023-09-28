f21f5ed26291e757ed3fabda2d1c3beb91df28f2
Bhu720 <146356327+Bhu720@users.noreply.github.com>
2023-09-28 19:39:20 +0530

    Add files via upload

---
+++ new.lpy.py
@@ -0,0 +1,21 @@
+passward = input("passward....> ")
+
+if passward == 'Bhudev':
+
+
+    print("Welcome Amrita Hospital")
+
+
+elif passward =='Amrita@123':
+
+
+    print("Access granted")
+
+
+else:
+    print("Wrong passward")
+
+
+    print("weelcome to amrita Hospital")
+    while true:
+        print("Try again")
