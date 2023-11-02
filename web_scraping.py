da637ef5aa97f90de6c61b6eeca6657ef1bdf80a
Bhu720 <146356327+Bhu720@users.noreply.github.com>
2023-11-02 17:11:15 +0530

    Add files via upload

--- web_scraping.py
+++ web_scraping.py
@@ -1,43 +1,28 @@
-import requests, webbrowser
-
-from bs4 import BeautifulSoup
-
-user_input = input("Enter something to search.. ")
-
-print("google....")
-
-google_search = requests.get("https://www.google.com/search?q=",user_input)
-
-soup = BeautifulSoup(google_search.text, "html.parser")
-
-print(soup.prettify())
-
-
+# import requests
+# from bs4 import BeautifulSoup
+# url = 'https://twitter.com'
+# resource = requests.get(url)
+# # print(resource.status_code)
+#
+# soup = BeautifulSoup(resource.text, 'html.parser')
+# print(soup)
 
 import requests
+from bs4 import BeautifulSoup
+twitter_username = 'twitter_username'
 
-# from bs4 import BeautifulSoup
-#
-# # url = ("https://www.bing.com/search?q=google+translat")
-# url = ("https://www.google.com/search?q=apple")
-#
-# r = requests.get(url)
-#
-# htmlcontent = r.content
-#
-# Soup = BeautifulSoup(htmlcontent,'html.parser')
-#
-# print(Soup)
+url = f'https://twitter.com/{twitter_username}'
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
+response = requests.get(url, headers=headers)
 
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    tweets = soup.find_all('p', class_='tweet-text')
 
-
-
-
-
-
-
-
-
+    for tweet in tweets:
+        print(tweet.get_text())
+else:
+    print(f"Failed to retrieve data from {url}. Status code: {response.status_code}")
